77b43747871d2f5c0a033f31b8cfcf814460608b
joshunrau <contact@joshuaunrau.com>
2024-03-08 15:35:43 -0500

    achieve 100% coverage

--- vitest.config.js
+++ vitest.config.js
@@ -3,8 +3,15 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      exclude: ['src/index.ts'],
       include: ['src/**/*'],
-      provider: 'v8'
+      provider: 'v8',
+      thresholds: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100
+      }
     }
   }
 });
