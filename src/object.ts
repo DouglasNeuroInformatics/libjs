7929ff5e1fa8c917d63240d24ed832b2b1167eb5
joshunrau <contact@joshuaunrau.com>
2024-04-10 21:33:28 -0400

    feat: add readonlyType option to deepFreeze

--- src/object.ts
+++ src/object.ts
@@ -2,11 +2,14 @@ export type ReadonlyDeep<T extends object> = Readonly<{
   [K in keyof T]: T[K] extends object ? ReadonlyDeep<T[K]> : T[K];
 }>;
 
-export function deepFreeze<T extends object>(obj: T): ReadonlyDeep<T> {
+export function deepFreeze<T extends object, TReadonly extends boolean = true>(
+  obj: T,
+  options?: { readonlyType: TReadonly }
+): TReadonly extends true ? ReadonlyDeep<T> : T {
   Object.keys(obj).forEach((key) => {
     const value: unknown = obj[key as keyof T];
     if (value && typeof value === 'object') {
-      deepFreeze(value);
+      deepFreeze(value, options);
     }
   });
   return Object.freeze(obj);
