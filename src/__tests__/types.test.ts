c41c4f2bf93408c9d61934741d67d725cc6bd144
joshunrau <contact@joshuaunrau.com>
2025-02-25 13:38:32 -0500

    feat: add never type helpers

--- src/__tests__/types.test.ts
+++ src/__tests__/types.test.ts
@@ -1,7 +1,22 @@
 import { expectTypeOf, test } from 'vitest';
 
-import type { ToAbstractConstructor } from '../types.js';
+import type { IsUnion, SingleKeyMap, ToAbstractConstructor } from '../types.js';
 
 test('ToAbstractConstructor', () => {
   expectTypeOf<ToAbstractConstructor<new () => { foo: string }>>().toEqualTypeOf<abstract new () => { foo: string }>();
+});
+
+test('IsUnion', () => {
+  expectTypeOf<IsUnion<string>>().toEqualTypeOf<false>();
+  expectTypeOf<IsUnion<number | string>>().toEqualTypeOf<true>();
+  expectTypeOf<IsUnion<1 | 2>>().toEqualTypeOf<true>();
+  expectTypeOf<IsUnion<1>>().toEqualTypeOf<false>();
+  expectTypeOf<IsUnion<'bar' | 'foo'>>().toEqualTypeOf<true>();
+  expectTypeOf<IsUnion<'foo'>>().toEqualTypeOf<false>();
+});
+
+test('SingleKeyMap', () => {
+  expectTypeOf<SingleKeyMap<string, number>>().toBeNever();
+  expectTypeOf<SingleKeyMap<'bar' | 'foo', number>>().toBeNever();
+  expectTypeOf<SingleKeyMap<'foo', number>>().toEqualTypeOf<{ foo: number }>();
 });
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import type { IfNever } from 'type-fest';
+import type { IfNever, IsStringLiteral } from 'type-fest';
 
 /**
  * Return whether `T` or any record nested therein contains the key `K`
@@ -31,3 +31,16 @@ export type ToAbstractConstructor<T extends new (...args: any[]) => any> = T ext
 ) => infer TReturn
   ? abstract new (...args: TArgs) => TReturn
   : never;
+
+export type IsUnion<T, U = T> = (T extends U ? (U extends T ? true : false) : never) extends true ? false : true;
+
+export type SingleKeyMap<K extends PropertyKey, V> =
+  IsStringLiteral<K> extends true
+    ? IsUnion<K> extends false
+      ? {
+          [key in K]: V;
+        }
+      : never
+    : never;
+
+export type B = SingleKeyMap<'a' | 'b', number>;
