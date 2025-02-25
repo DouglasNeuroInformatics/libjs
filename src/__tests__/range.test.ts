c7607e1d7842cd2b004b6646ce7b32d3e778407e
joshunrau <contact@joshuaunrau.com>
2025-02-25 14:32:47 -0500

    refactor: add neverthrow to range
    BREAKING CHANGE

--- src/__tests__/range.test.ts
+++ src/__tests__/range.test.ts
@@ -4,10 +4,10 @@ import { range } from '../range.js';
 
 describe('range', () => {
   it('should return an array equal in length to the range', () => {
-    const arr = range(10);
+    const arr = range(10)._unsafeUnwrap();
     expect(arr.length).toBe(10);
   });
-  it('should throw an error if the start is equal to the end', () => {
-    expect(() => range(1, 1)).toThrow();
+  it('should return an error if the start is equal to the end', () => {
+    expect(range(1, 1).isErr()).toBe(true);
   });
 });
