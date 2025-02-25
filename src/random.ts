82ec6867e0c1da715dfabe5206e5b03a756d738c
joshunrau <contact@joshuaunrau.com>
2025-02-25 12:35:17 -0500

    refactor: add result types for random module
    BREAKING CHANGE

--- src/random.ts
+++ src/random.ts
@@ -1,23 +1,30 @@
+import { ok } from 'neverthrow';
+import type { Result } from 'neverthrow';
+
+import { ValueException } from './exception.js';
+
 /** Returns a random integer between `min` (inclusive) and `max` (not inclusive) */
-export function randomInt(min: number, max: number): number {
+export function randomInt(min: number, max: number): Result<number, typeof ValueException.Instance> {
   if (min >= max) {
-    throw new Error(`Min value '${min}' must not be greater than or equal to the max value '${max}'`);
+    return ValueException.asErr(`Min value '${min}' must not be greater than or equal to the max value '${max}'`);
   }
-  return Math.floor(Math.random() * (max - min)) + min;
+  return ok(Math.floor(Math.random() * (max - min)) + min);
 }
 
 /** Returns a random date between `start` and `end` (both inclusive) */
-export function randomDate(start: Date, end: Date): Date {
+export function randomDate(start: Date, end: Date): Result<Date, typeof ValueException.Instance> {
   if (start > end) {
-    throw new Error(`Start date '${start.toISOString()}' cannot be greater than end date '${end.toISOString()}'`);
+    return ValueException.asErr(
+      `Start date '${start.toISOString()}' cannot be greater than end date '${end.toISOString()}'`
+    );
   }
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  return ok(new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())));
 }
 
 /** Returns a random value from the array */
-export function randomValue<T>(arr: T[]): T {
+export function randomValue<T>(arr: T[]): Result<T, typeof ValueException.Instance> {
   if (arr.length === 0) {
-    throw new Error('Cannot select random value from array of length zero');
+    return ValueException.asErr('Cannot select random value from array of length zero');
   }
-  return arr[randomInt(0, arr.length)]!;
+  return randomInt(0, arr.length).map((index) => arr[index]!);
 }
